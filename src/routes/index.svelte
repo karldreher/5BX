1d6919c8ab5caa860e30ada98a5fc86d1a811e52
Karl Dreher <karldreher@outlook.com>
2021-06-10 19:21:55 -0700

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+    let rungs = [...Array(55).keys()];
+    
+    async function getRung() {
+
+        const result = await fetch("rung/get1");
+        
+    }
+    
+</script>
+
+
+<select id='rungSelector'>
+	{#each rungs as rung}
+	<option value={rung}>{rung}</option>
+	{/each}
+</select>
+
+<button on:click={getRung()}>Click</button>
+{#each result as r}
+<h2 id='display'>{r}</h2>
+{/each}
